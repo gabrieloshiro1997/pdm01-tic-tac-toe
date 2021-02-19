ae34533e8f65ec82d10d5c413bae4dddf3150ff5
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-02-19 00:10:30 -0300

    feat: add winning player

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -5,7 +5,7 @@ import { Square } from '../Square';
 import './styles.css';
 
 function Board(props) {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(''));
   const [xIsNext, setXIsNext] = useState(true);
 
   const renderSquare = (i) => {
@@ -26,9 +26,38 @@ function Board(props) {
     return player;
   }, [xIsNext]);
 
+  const winningPlayer = useMemo(() => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (
+        squares[a].toString() &&
+        squares[a].toString() === squares[b].toString() &&
+        squares[b].toString() === squares[c].toString()
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }, [squares]);
+
   return (
     <div>
-      <div className='status'>Jogador: {nextPlayer}</div>
+      {winningPlayer && (
+        <div className='status'>Jogador vencedor: {winningPlayer}</div>
+      )}
+
+      {!winningPlayer && <div className='status'>Jogador: {nextPlayer}</div>}
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
