3953c434dfd719ac6fd62bce8ae518b9081e6660
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-02-18 22:15:28 -0300

    feature: add board component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-import { Square } from './components/Square';
+import { Board } from './components/Board';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Square value={0} onClick={() => console.log('0')} />
+    <Board
+      squares={Array(9)
+        .fill()
+        .map((_, index) => index)}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
