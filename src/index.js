e6fdbd12de02c25bcba4067f69a3659c2193a6ea
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-02-18 21:44:19 -0300

    feat: add square component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+import { Square } from './components/Square';
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <Square value={0} onClick={() => console.log('0')} />
   </React.StrictMode>,
   document.getElementById('root')
 );
